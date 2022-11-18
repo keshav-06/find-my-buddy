f629bbd4ac53694b2514de079a731471efe99191
Aryan <aryan.khubchandani@gmail.com>
2022-11-18 19:54:49 +0530

    feat: update frontend

---
+++ server/controllers/chatRoom.js
@@ -0,0 +1,7 @@
+export default {
+  initiate: async (req, res) => {},
+  postMessage: async (req, res) => {},
+  getRecentConversation: async (req, res) => {},
+  getConversationByRoomId: async (req, res) => {},
+  markConversationReadByRoomId: async (req, res) => {},
+};
